af99b9e60f1d778f9877680331f8047965c16538
oradocaj <oradocaj@gmail.com>
2021-07-28 21:44:22 +0200

    finishing project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,22 @@
-import React from 'react';
-//import logo from '../../logo.svg';
+import React, { Component } from 'react';
+import logo from '../../logo.svg';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 import '../BusinessList/BusinessList';
 import '../SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-        {< SearchBar />}
-        {<BusinessList />} 
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+          {<SearchBar />}
+          {<BusinessList />} 
+      </div>
+    );
+  }
+  
 };
 
 export default App;
-
-/* <div className="App">
-<header className="App-header">
-<img src={logo} className="App-logo" alt="logo" />
-<p>
-  Edit <code>src/App.js</code> and save to reload.
-</p>
-<a
-  className="App-link"
-  href="https://reactjs.org"
-  target="_blank"
-  rel="noopener noreferrer"
->
-  Learn React
-</a>
-</header>
-</div>
-);
-};
-*/
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -12,27 +12,25 @@ class SearchBar extends React.Component {
    renderSortByOptions () {
     return Object.keys(sortByOptions).map(sortByOption => {
         let sortByOptionValue = sortByOptions[sortByOption];
-        return (
-            <li key={sortByOptionValue}>{sortByOption}</li>
-        )
+        return <li key={sortByOptionValue}>{sortByOption}</li>        
     });
    }
    render() {
        return (
         <div className="SearchBar">
-        <div className="SearchBar-sort-options">
-          <ul>
-            {renderSortByOptions()}
-          </ul>
+          <div className="SearchBar-sort-options">
+            <ul>
+              {this.renderSortByOptions()}
+            </ul>
+          </div>
+          <div className="SearchBar-fields">
+            <input placeholder="Search Businesses" />
+            <input placeholder="Where?" />
+          </div>
+          <div className="SearchBar-submit">
+            <a>Let's Go</a>
+          </div>
         </div>
-        <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" />
-          <input placeholder="Where?" />
-        </div>
-        <div className="SearchBar-submit">
-          <a>Let's Go</a>
-        </div>
-      </div>
        )
    }
 }
