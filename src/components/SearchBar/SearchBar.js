452ddd6b5a70df889beb82aca85c06707c4cfd5e
Ognjen Radočaj <oradocaj@team.nova.hr>
2021-08-25 15:57:03 +0200

    added handleKeyPress method

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import './SearchBar.css';
 
-
 class SearchBar extends React.Component {
 
   constructor (props) {
@@ -11,13 +10,14 @@ class SearchBar extends React.Component {
     this.state = {
       term: '',
       location: '',
-      sortBy: 'best_match'
+      sortBy: 'best_match'      
     };
 
     this.handleTermChange = this.handleTermChange.bind(this);
     this.handleLocationChange = this.handleLocationChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
-    this.handleSortByChange = this.handleSortByChange.bind(this);
+    this.handleSortByChange = this.handleSortByChange.bind(this);    
+    this.handleKeyPress = this.handleKeyPress.bind(this);
     
     this.sortByOptions = {
       'Best Match':'best_match',
@@ -54,18 +54,31 @@ class SearchBar extends React.Component {
 
   handleSearch (event) {
     this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
-
+    
     event.preventDefault();
   }
 
+  handleKeyPress (event) {
+    if (event.key === 'Enter') {
+      console.log('Enter clicked!')
+      this.btn.click();
+    };
+  }
+
+  /*handleSortKeyPress (event) {
+    if (event.onClick) {      
+      this.btn.click();
+    };
+  }
+  */
   renderSortByOptions () {
     return Object.keys(this.sortByOptions).map(sortByOption => {
       let sortByOptionValue = this.sortByOptions[sortByOption];
-      return <li onClick={this.handleSortByChange.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>        
+      return <li onClick={this.handleSortByChange.bind(this, sortByOptionValue)}  className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>        
     });
   }
 
-  render() {
+  render() {    
     return (
       <div className="SearchBar">
         <div className="SearchBar-sort-options">
@@ -75,10 +88,10 @@ class SearchBar extends React.Component {
         </div>
         <div className="SearchBar-fields">
           <input placeholder="Search Businesses" onChange={this.handleTermChange} />
-          <input placeholder="Where?" onChange={this.handleLocationChange} />
+          <input placeholder="Where?" onChange={this.handleLocationChange} onKeyPress={this.handleKeyPress} />         
         </div>
         <div className="SearchBar-submit">
-          <a onClick={this.handleSearch}>Let's Go</a>
+          <a onClick={this.handleSearch} ref={node => (this.btn = node)}>Let's Go</a>
         </div>
       </div>
     );
